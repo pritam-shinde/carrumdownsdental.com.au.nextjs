c1944ea59348deb2cb8b1317445dd8a651ea4ac6
Pritam Shinde <44087724+pritam-shinde@users.noreply.github.com>
2022-11-11 16:56:08 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -15,67 +15,63 @@ function MyApp({ Component, pageProps }) {
   return (<>
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="google-site-verification" content="jT613v_6huP5m9TPQAwkZI-Iehyg2TBmgOnavXJAeeA" />
-          <script type="application/ld+json" id="schema">
-        {
-          JSON.stringify({
-            "@context": "https://schema.org",
-            "@type": "Dentist",
-            "name": "Carrum Downs Dental Group",
-            "image": "https://d3kk1vdl9fuovr.cloudfront.net/wp-content/uploads/2016/06/CD_logo_lanscape.png",
-            "@id": "",
-            "url": "https://carrumdownsdental.com.au/",
-            "telephone": "0397821200",
-            "priceRange": "$$",
-            "address": {
-              "@type": "PostalAddress",
-              "streetAddress": "1095 Frankston-Dandenong Rd",
-              "addressLocality": "Carrum Downs",
-              "addressRegion": "VIC",
-              "postalCode": "3201",
-              "addressCountry": "AU"
-            },
-            "aggregateRating":
-            {
-              "@type": "AggregateRating",
-              "ratingValue": "4.9",
-              "reviewCount": "80"
-            },
-            "geo": {
-              "@type": "GeoCoordinates",
-              "latitude": -38.0886367,
-              "longitude": 145.1825171
-            },
-            "openingHoursSpecification": [{
-              "@type": "OpeningHoursSpecification",
-              "dayOfWeek": [
-                "Monday",
-                "Tuesday",
-                "Saturday"
-              ],
-              "opens": "09:00",
-              "closes": "18:00"
-            }, {
-              "@type": "OpeningHoursSpecification",
-              "dayOfWeek": [
-                "Wednesday",
-                "Thursday",
-                "Friday"
-              ],
-              "opens": "08:30",
-              "closes": "17:00"
-            }],
-            "sameAs": [
-              "https://www.facebook.com/CarrumDownsDental/",
-              "https://twitter.com/CarrumDownsGrp",
-              "https://www.instagram.com/cddentalgroup/",
-              "https://www.youtube.com/channel/UCqG1nDBsBULhWGi00Twe7rg",
-              "https://www.pinterest.com/CarrumDownsGrp/"
-            ]
-          })
-        }
-      </script>
+      <meta name="google-site-verification" content="jT613v_6huP5m9TPQAwkZI-Iehyg2TBmgOnavXJAeeA" /
     </Head>
+        
+          <script type="application/ld+json">{`
+{
+                  "@context": "https://schema.org",
+                  "@type": "Dentist",
+                  "name": "Carrum Downs Dental Group",
+                  "image": "https://d3kk1vdl9fuovr.cloudfront.net/wp-content/uploads/2016/06/CD_logo_lanscape.png",
+                  "@id": "",
+                  "url": "https://carrumdownsdental.com.au/",
+                  "telephone": "0397821200",
+                  "priceRange": "$$",
+                  "address": {
+                        "@type": "PostalAddress",
+                        "streetAddress": "Shop, T5/100 Hall Rd",
+                        "addressLocality": "Carrum Downs",
+                        "addressRegion": "VIC",
+                        "postalCode": "3201",
+                        "addressCountry": "AU"
+                  },
+                "aggregateRating":
+                {
+                       "@type": "AggregateRating",
+                       "ratingValue": "4.9",
+                       "reviewCount": "91"
+                },
+                  "geo": {
+                        "@type": "GeoCoordinates",
+                        "latitude": -38.0886367,
+                        "longitude": 145.1825171
+                  },
+                  "openingHoursSpecification": [{
+                        "@type": "OpeningHoursSpecification",
+                        "dayOfWeek": [
+                          "Monday",
+                          "Tuesday",
+			  "Wednesday",
+                          "Thursday",
+                          "Friday",
+                          "Saturday"
+                        ],
+                        "opens": "09:00",
+                        "closes": "18:00"
+			
+                 
+                  }
+			],
+                  "sameAs": [
+                        "https://www.facebook.com/CarrumDownsDental/",
+                        "https://twitter.com/CarrumDownsGrp",
+                        "https://www.instagram.com/cddentalgroup/",
+                        "https://www.youtube.com/channel/UCqG1nDBsBULhWGi00Twe7rg",
+                        "https://www.pinterest.com.au/CarrumDownsGrp/"
+                  ] 
+                }`}
+</script> 
           
     <Script id="google-tag-manager" strategy="afterInteractive">
       {
