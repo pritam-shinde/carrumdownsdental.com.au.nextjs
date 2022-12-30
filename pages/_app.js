8749523d83b0a26368e8133f1fc45659b5214ce8
Pritam Shinde <44087724+pritam-shinde@users.noreply.github.com>
2022-12-30 18:29:21 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -9,9 +9,29 @@ import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
+   useEffect(() => {
+    window.myInfo = () => {
+      const developer = [
+        {
+          Developer: "Aditya Nitin Burse (Jain)",
+          Designation: "Front-End Developer",
+          Employer: "Nonstop Corporation"
+        },
+        {
+          Developer: "Ankit Shahu",
+          Designation: "Full Stack Developer",
+          Employer: "Nonstop Corporation"
+        }
+
+      ]
+      console.log(developer)
+    }
+  })
   useEffect(() => {
     import("bootstrap/dist/js/bootstrap.bundle");
   }, []);
+  
+  
   
   return (<>
     <Head>
