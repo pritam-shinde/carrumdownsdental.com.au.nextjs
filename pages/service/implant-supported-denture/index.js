1186b1b6fbcd3c4a3b24b686aedc2e0c5002e9d5
Pritam Shinde <44087724+pritam-shinde@users.noreply.github.com>
2023-04-07 13:57:12 +0530

    Implant Supported Denture updated name

--- pages/service/implant-supported-denture/index.js
+++ pages/service/implant-supported-denture/index.js
@@ -36,7 +36,7 @@ const Implant = () => {
   return (
     <>
       <Head>
-        <title> Implant supported denture in Carrum Downs | Carrum Downs Dental Group</title>
+        <title> Implant Supported Denture in Carrum Downs | Carrum Downs Dental Group</title>
         <meta name="description" content="Are you looking to achieve the best smile? Carrum Downs Dental offers Smile Design service that uses a digital process to fix your smile. Contact us at 03-9782 1200." />
         <meta name="robots" content="noindex" />
       </Head>
