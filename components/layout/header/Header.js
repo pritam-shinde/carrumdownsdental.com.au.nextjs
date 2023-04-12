649851e7b14105fb547c9a137a84fbe313303be3
Pritam Shinde <44087724+pritam-shinde@users.noreply.github.com>
2023-04-12 17:06:35 +0530

    Update Header.js

--- components/layout/header/Header.js
+++ components/layout/header/Header.js
@@ -35,12 +35,12 @@ const Header = () => {
     <>
       <header className={Styles.header}>
 
-           <Marquee speed={50} className='bg-white'><span className='ms-5 me-5 text-danger'><b>We have moved to a Bigger and Shinier location within Carrum Downs with plenty of easy parking</b></span><span className='ms-5 me-5 text-danger'><b>We have moved to a Bigger and Shinier location within Carrum Downs with plenty of easy parking</b></span><span className='ms-5 me-5 text-danger'><b>We have moved to a Bigger and Shinier location within Carrum Downs with plenty of easy parking</b></span><span className='ms-5 me-5 text-danger'><b>We have moved to a Bigger and Shinier location within Carrum Downs with plenty of easy parking</b></span></Marquee>
+        <Marquee speed={50} className='bg-white'><span className='ms-5 me-5 text-danger'><b>We have moved to a Bigger and Shinier location within Carrum Downs with plenty of easy parking</b></span><span className='ms-5 me-5 text-danger'><b>We have moved to a Bigger and Shinier location within Carrum Downs with plenty of easy parking</b></span><span className='ms-5 me-5 text-danger'><b>We have moved to a Bigger and Shinier location within Carrum Downs with plenty of easy parking</b></span><span className='ms-5 me-5 text-danger'><b>We have moved to a Bigger and Shinier location within Carrum Downs with plenty of easy parking</b></span></Marquee>
           <Grid container>
             <Grid item xs={12} md={10} className="mx-auto">
               <Topbar />
               <Navbar />
-            </Grid>
+           </Grid>
           </Grid>
         </Container>
       </header>
