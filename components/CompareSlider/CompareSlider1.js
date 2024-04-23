8a206965beaf3fbb4ac23c77a0a9052381161f62
Pritam Shinde <44087724+pritam-shinde@users.noreply.github.com>
2024-04-23 16:47:40 +0530

    added image slider images

--- components/CompareSlider/CompareSlider1.js
+++ components/CompareSlider/CompareSlider1.js
@@ -1,3 +1,31 @@
+import React, { useState } from "react";
+import ReactCompareImage from "react-compare-image";
+
+const CompareSlider1 = () => {
+  const [hasError, setHasError] = useState(false);
+
+  const handleOnError = (error, errorInfo) => {
+    console.log("Error occurred:", error, errorInfo);
+    setHasError(true);
+  };
+
+  return (
+    <div className="mainWrapper">
+      <div className="contentWrapper">
+        {hasError ? (
+          <div>Error occurred while loading images.</div>
+        ) : (
+          <>
+            <ReactCompareImage leftImage="/Veneers/slider-images/before-slider1.jpg" rightImage="/Veneers/slider-images/after-slider1.jpg" alt="slider"/>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default CompareSlider1;
+
 // import React, { useState } from "react";
 // // import "../../styles/compareSlider.css";
 
@@ -103,34 +131,7 @@
 
 // export default CompareSlider1;
 
-import React, { useState } from "react";
-import ReactCompareImage from "react-compare-image";
 
-const CompareSlider1 = () => {
-  const [hasError, setHasError] = useState(false);
-
-  const handleOnError = (error, errorInfo) => {
-    console.log("Error occurred:", error, errorInfo);
-    setHasError(true);
-  };
-
-  return (
-    <div className="mainWrapper">
-      <div className="contentWrapper">
-        {hasError ? (
-          <div>Error occurred while loading images.</div>
-        ) : (
-          <>
-            <ReactCompareImage leftImage="/Veneers/slider-images/before-slider1.jpg" rightImage="/Veneers/slider-images/after-slider1.jpg" alt="slider"/>
-            {/* <ReactCompareImage leftImage="/Veneers/veneers-before-img.jpg" rightImage="/Veneers/veneers-after-img.jpg" alt="slider"/> */}
-          </>
-        )}
-      </div>
-    </div>
-  );
-};
-
-export default CompareSlider1;
 
 // import React, { useState } from "react";
 
